ad3f8d56dbb22a83232a57c116176ba6fc0cf41b
kusumadwi <37705698+kusumadwiy@users.noreply.github.com>
2024-07-05 03:28:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ model = pickle.load(open('model.pkl', 'rb'))
 with open('encoder.pkl', 'rb') as encoder_file:
     encoder = pickle.load(encoder_file)
 
-
-encoder = OneHotEncoder(sparse=False, drop='first', handle_unknown='ignore')
-
 st.title("Prediksi Penjualan Produk")
 
 st.write("Masukkan detail untuk mendapatkan prediksi penjualan:")
