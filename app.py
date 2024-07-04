22a0df98f14a0ccaa075abefd6e6516ebd9ea8d8
kusumadwi <37705698+kusumadwiy@users.noreply.github.com>
2024-07-04 19:05:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pickle
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
 
 # Load model dan encoder dari file
 model = pickle.load(open('model.pkl', 'rb'))
@@ -19,8 +18,8 @@ with st.form(key='prediction_form'):
     year = st.number_input('Year', min_value=2000, max_value=2100, step=1)
     gender = st.selectbox('Gender', options=['Male', 'Female'])
     age = st.number_input('Age', min_value=0, max_value=100, step=1)
-    category = st.selectbox('Product Category', options=['Beauty', 'Clothing', 'Electronics'])
-    spending = st.number_input('Total Spending', min_value=100, step=5)
+    category = st.selectbox('Product Category', options=['Beauty', 'Category2', 'Category3'])
+    spending = st.number_input('Total Spending', min_value=100, step=50)
     
     submit_button = st.form_submit_button(label='Predict')
 
@@ -38,7 +37,14 @@ if submit_button:
     try:
         # Encode data baru dengan encoder yang sama
         encoded_new_data = encoder.transform(new_data[['Gender', 'Product Category']])
-        encoded_columns = encoder.get_feature_names(['Gender', 'Product Category'])
+        
+        # Retrieve the feature names
+        if hasattr(encoder, 'get_feature_names_out'):
+            encoded_columns = encoder.get_feature_names_out(['Gender', 'Product Category'])
+        else:
+            # Handle versions where get_feature_names_out might not be available
+            encoded_columns = encoder.get_feature_names(['Gender', 'Product Category'])
+        
         encoded_new_data = pd.DataFrame(encoded_new_data, columns=encoded_columns)
         final_new_data = pd.concat([new_data[['Month', 'Year', 'Age', 'Total Spending']], encoded_new_data], axis=1)
 
