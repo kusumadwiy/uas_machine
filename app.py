eeeb9204775ddde264f699da7eb74305f62d449a
kusumadwi <37705698+kusumadwiy@users.noreply.github.com>
2024-07-09 02:29:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import pickle
 
 # Function to load the model
 def load_model(model_path):
-    model = pickle.load(model_path)
+    with open(model_path, 'rb') as file:
+        model = pickle.load(file)
     return model
 
 # Load the saved model
@@ -16,12 +17,12 @@ def predict(model, input_data):
     prediction = model.predict(input_data)
     return prediction
 
-
+# Streamlit App
 def main():
     st.title('Sales Prediction App')
     st.sidebar.title('Input Parameters')
 
-
+    # Input form for user
     month = st.sidebar.selectbox('Month', range(1, 13))
     year = st.sidebar.selectbox('Year', range(2000, 2025))
     gender = st.sidebar.selectbox('Gender', ['Male', 'Female'])
@@ -29,6 +30,7 @@ def main():
     product_category = st.sidebar.selectbox('Product Category', ['Beauty', 'Clothing', 'Electronics'])
     total_spending = st.sidebar.number_input('Total Spending')
 
+    # Prepare input data as DataFrame
     input_data = {
         'Month': [month],
         'Year': [year],
@@ -37,10 +39,8 @@ def main():
         'Product Category': [product_category],
         'Total Spending': [total_spending]
     }
-    
-    # Buat DataFrame dari data input
     input_df = pd.DataFrame(input_data)
-    
+
     # Encoding categorical variables
     input_encoded = pd.get_dummies(input_df, columns=['Gender', 'Product Category'], drop_first=False)
 
